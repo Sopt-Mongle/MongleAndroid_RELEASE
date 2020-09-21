a177a513d41d0ab9c64b5530eea22ac4b38064c4
jinyand <jiiin97@naver.com>
2020-09-21 22:19:00 +0900

    [Implement Features] 회원가입 1단계 기능 구현 완료

--- app/src/main/java/com/example/mongleandroid_release/activity/JoinStep1Activity.kt
+++ app/src/main/java/com/example/mongleandroid_release/activity/JoinStep1Activity.kt
@@ -1,9 +1,9 @@
 package com.example.mongleandroid_release.activity
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mongleandroid_release.R
 import kotlinx.android.synthetic.main.activity_join_step1.*
@@ -23,9 +23,30 @@ class JoinStep1Activity : AppCompatActivity() {
 
         // 회원가입 2단계로 이동
         activity_join_step1_btn_next.setOnClickListener {
-            val intent = Intent(this, JoinStep2Activity::class.java)
-            startActivity(intent)
+            if(activity_join_step1_cb.isChecked) {
+                val intent = Intent(this, JoinStep2Activity::class.java)
+                startActivity(intent)
+            } else {
+                activity_join_step1_img_warning.visibility = VISIBLE
+                activity_join_step1_tv_warning.visibility = VISIBLE
+            }
         }
+
+        // 체크박스 여부에 따른 경고문구 표시 & 프로그래스바 색상 변경
+        activity_join_step1_cb.setOnClickListener {
+            if(activity_join_step1_cb.isChecked) {
+                activity_join_step1_img_warning.visibility = GONE
+                activity_join_step1_tv_warning.visibility = GONE
+                activity_join_step1_pgb_out.setBackgroundResource(R.drawable.dot_circle_progresson_out)
+                activity_join_step1_pgb_in.setBackgroundResource(R.drawable.dot_circle_progresson_in)
+            } else {
+                activity_join_step1_pgb_out.setBackgroundResource(R.drawable.dot_circle_progressoff_out)
+                activity_join_step1_pgb_in.setBackgroundResource(R.drawable.dot_circle_progressoff_in)
+            }
+        }
+
+
+
 
 
     }
