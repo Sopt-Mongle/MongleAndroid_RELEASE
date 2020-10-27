e69c3a4139b50e59d2f9e3d00a4f7c491f405d2b
jinyand <jiiin97@naver.com>
2020-10-27 14:27:53 +0900

    [Implement Features] 큐레이터 키워드뷰 연결

--- app/src/main/java/com/example/mongleandroid_release/fragment/CuratorFragment.kt
+++ app/src/main/java/com/example/mongleandroid_release/fragment/CuratorFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mongleandroid_release.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mongleandroid_release.R
+import com.example.mongleandroid_release.activity.CuratorKeywordActivity
 import com.example.mongleandroid_release.adapter.CuratorRecommendAdapter
 import com.example.mongleandroid_release.network.RequestToServer
 import com.example.mongleandroid_release.network.customEnqueue
@@ -29,6 +31,28 @@ class CuratorFragment : Fragment() {
         curatorRecommend()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_curator, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        goKeywordActivity(fragment_curator_tv_gamsung, 1)
+        goKeywordActivity(fragment_curator_tv_donggi, 2)
+        goKeywordActivity(fragment_curator_tv_jagi, 3)
+        goKeywordActivity(fragment_curator_tv_kipeun, 4)
+        goKeywordActivity(fragment_curator_tv_dokseo, 5)
+        goKeywordActivity(fragment_curator_tv_ilsang, 6)
+
+    }
+
+    private fun goKeywordActivity(view: View, value : Int) {
+        view.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, CuratorKeywordActivity::class.java)
+                intent.putExtra("params", value)
+                startActivity(intent)
+            }
+        }
     }
 
     private fun curatorRecommend() {
