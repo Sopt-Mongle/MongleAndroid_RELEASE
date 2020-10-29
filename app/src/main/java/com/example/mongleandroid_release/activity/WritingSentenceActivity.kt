704dc04ceb23575d35e5f820743cc9b31f3ead2d
sery270 <sery270@naver.com>
2020-10-29 23:32:26 +0900

    [Chore] SOS!!!!!!!

--- app/src/main/java/com/example/mongleandroid_release/activity/WritingSentenceActivity.kt
+++ app/src/main/java/com/example/mongleandroid_release/activity/WritingSentenceActivity.kt
@@ -1,12 +1,28 @@
 package com.example.mongleandroid_release.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.Nullable
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.mongleandroid_release.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class WritingSentenceActivity : AppCompatActivity() {
+    //    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+//    val navController = navHostFragment.navController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing_sentence)
+
+
+//        navController.setGraph(R.navigation.nav_writing_sentence)
+
+
     }
+
 }
--- app/src/main/java/com/example/mongleandroid_release/fragment/WritingSentenceFinishFragment.kt
+++ app/src/main/java/com/example/mongleandroid_release/fragment/WritingSentenceFinishFragment.kt
@@ -1,33 +1,21 @@
 package com.example.mongleandroid_release.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.mongleandroid_release.R
+import com.example.mongleandroid_release.activity.MainActivity
+import com.example.mongleandroid_release.activity.WritingSentenceActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WritingSentenceFinishFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WritingSentenceFinishFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val act : WritingSentenceActivity = context as WritingSentenceActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +23,23 @@ class WritingSentenceFinishFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.writing_sentence_finish, container, false)
+//
+
+//        act.finish()
+        val view =  inflater.inflate(R.layout.writing_sentence_finish, container, false)
+        view.findViewById<Button>(R.id.writing_sentence_finish_btn_sentence).setOnClickListener {
+            val intent = Intent(it.context , MainActivity::class.java)
+            it.context.startActivity(intent)
+            act.finish()
+        }
+        view.findViewById<Button>(R.id.writing_sentence_finish_btn_main).setOnClickListener {
+            //나중에 문장 확인하는 뷰 만들어지면 수정해야함 !
+            val intent = Intent(it.context , MainActivity::class.java)
+            it.context.startActivity(intent)
+            act.finish()
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WritingSentenceFinishFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WritingSentenceFinishFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
