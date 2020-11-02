fdedc9110f634a653f215d652299284ca0cf5d53
chloe73 <esom735@gmail.com>
2020-11-02 22:19:55 +0900

    [Create UI] - 테마 상세보기 작업 중..

---
+++ app/src/main/java/com/example/mongleandroid_release/activity/DetailThemeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mongleandroid_release.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mongleandroid_release.R
+
+class DetailThemeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_theme)
+
+
+    }
+}
