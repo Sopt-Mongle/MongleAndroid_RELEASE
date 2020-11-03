81ee622a36ec27cf07ef7e9871f1e8e2a751de6c
jinyand <jiiin97@naver.com>
2020-11-03 22:38:43 +0900

    [Create UI] 약관 및 운영정책 뷰

--- app/src/main/java/com/example/mongleandroid_release/activity/ServiceTermsActivity.kt
+++ app/src/main/java/com/example/mongleandroid_release/activity/ServiceTermsActivity.kt
@@ -3,15 +3,10 @@ package com.example.mongleandroid_release.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mongleandroid_release.R
-import kotlinx.android.synthetic.main.activity_join_step1_policy.*
 
-class JoinStep1PolicyActivity : AppCompatActivity() {
+class ServiceTermsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_step1_policy)
-
-        activity_join_policy_btn_back.setOnClickListener {
-            finish()
-        }
+        setContentView(R.layout.activity_service_terms)
     }
 }
