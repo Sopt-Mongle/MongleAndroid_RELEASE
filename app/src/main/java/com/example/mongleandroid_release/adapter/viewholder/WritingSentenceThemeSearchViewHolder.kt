9d9322857cfd82d61dcda47b39b1da320ba371d3
sery270 <sery270@naver.com>
2020-11-05 15:04:03 +0900

    [Implement Features] 테마 검 하이라이색팅 #44

--- app/src/main/java/com/example/mongleandroid_release/adapter/viewholder/WritingSentenceThemeSearchViewHolder.kt
+++ app/src/main/java/com/example/mongleandroid_release/adapter/viewholder/WritingSentenceThemeSearchViewHolder.kt
@@ -1,16 +1,55 @@
 package com.example.mongleandroid_release.adapter.viewholder
 
+import android.graphics.Color
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.mongleandroid_release.R
-import com.example.mongleandroid_release.network.data.response.ThemeData
+import com.example.mongleandroid_release.activity.MainActivity
+import com.example.mongleandroid_release.network.data.response.SearchTheme
 
 class WritingSentenceThemeSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(writingSentenceThemeSearchData: ThemeData){
-        itemView.findViewById<TextView>(R.id.item_writing_sentence_theme_result_tv_title).text = writingSentenceThemeSearchData.theme
-        itemView.findViewById<TextView>(R.id.item_writing_sentence_theme_result_tv_themeIdx).text = writingSentenceThemeSearchData.themeIdx.toString()
+    val theme = itemView.findViewById<TextView>(R.id.item_writing_sentence_theme_result_tv_title)
+    val themeIdx =
+        itemView.findViewById<TextView>(R.id.item_writing_sentence_theme_result_tv_themeIdx)
+    val themeImg = itemView.findViewById<ImageView>(R.id.item_writing_sentence_theme_result_img)
+
+    fun bind(searchTheme: SearchTheme) {
+
+        theme.text = searchTheme.theme
+        themeIdx.text = searchTheme.themeIdx.toString()
+        Glide.with(itemView).load(searchTheme.themeImg).into(themeImg)
+
+        val inputString = MainActivity.search_result
+
+        val sb = StringBuilder(inputString)
+        val startWord = sb[0]
+        var startIndex = searchTheme.theme.indexOf(startWord)
+        val spannable = SpannableStringBuilder(searchTheme.theme)
+
+        var index = 0
+        while (index < searchTheme.theme.length && startIndex != -1) {
+            startIndex = searchTheme.theme.indexOf(inputString, index)
+            if (startIndex == -1) break else {
+                spannable.setSpan(
+                    ForegroundColorSpan(Color.rgb(115, 192, 136)),
+                    startIndex,
+                    startIndex + inputString.length,
+                    Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+                )
+                theme.text = spannable
+            }
+            index = startIndex + 1
+        }
+
+        theme.text = spannable
+
 
     }
 }
