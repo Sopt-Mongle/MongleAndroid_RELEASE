7ac8269d1f6a5e833fd0f73efbc3c96aa69b3b75
jinyand <jiiin97@naver.com>
2020-11-06 15:32:18 +0900

    [Implement Features] 큐레이터 검색 구독여부 서버 연결

--- app/src/main/java/com/example/mongleandroid_release/adapter/SearchCuratorAdapter.kt
+++ app/src/main/java/com/example/mongleandroid_release/adapter/SearchCuratorAdapter.kt
@@ -2,11 +2,13 @@ package com.example.mongleandroid_release.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mongleandroid_release.R
 import com.example.mongleandroid_release.adapter.viewholder.SearchCuratorViewHolder
 import com.example.mongleandroid_release.network.data.response.SearchCurator
+import kotlinx.android.synthetic.main.item_search_curator.view.*
 
 class SearchCuratorAdapter(private val context : Context, var datas : List<SearchCurator>) : RecyclerView.Adapter<SearchCuratorViewHolder>() {
 
@@ -17,10 +19,35 @@ class SearchCuratorAdapter(private val context : Context, var datas : List<Searc
 
     override fun onBindViewHolder(holder: SearchCuratorViewHolder, position: Int) {
         holder.bind(datas[position])
+
+        holder.itemView.search_curator_btn_subscribe.isChecked = datas[position].alreadySubscribed
+        if(datas[position].alreadySubscribed) holder.itemView.search_curator_btn_subscribe.text = "구독중"
+
+        holder.itemView.search_curator_btn_subscribe.setOnCheckedChangeListener { view, isChecked ->
+            itemClickListener.onClickSubscribe(view, position)
+        }
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClickItem(it, position)
+        }
     }
 
     override fun getItemCount(): Int {
         return datas.size
+    }
+
+    //클릭 인터페이스 정의
+    interface ItemClickListener {
+        fun onClickItem(view: View, position: Int)
+        fun onClickSubscribe(view: View, position: Int)
+    }
+
+    //클릭리스너 선언
+    private lateinit var itemClickListener: ItemClickListener
+
+    //클릭리스너 등록 메소드
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListener = itemClickListener
     }
 
 }
