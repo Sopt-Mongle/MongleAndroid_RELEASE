93eba7cc3dad922b88f8d2f66b4c87fe5f2ffd72
sery270 <sery270@naver.com>
2020-11-11 21:34:05 +0900

    [Implement Features] writing_sentence_finish_btn_sentence 연결 #40

--- app/src/main/java/com/example/mongleandroid_release/fragment/WritingSentenceFinishFragment.kt
+++ app/src/main/java/com/example/mongleandroid_release/fragment/WritingSentenceFinishFragment.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import androidx.navigation.findNavController
 import com.example.mongleandroid_release.R
 import com.example.mongleandroid_release.activity.MainActivity
+import com.example.mongleandroid_release.activity.SentenceDetailViewActivity
 import com.example.mongleandroid_release.activity.WritingSentenceActivity
 
 class WritingSentenceFinishFragment : Fragment() {
@@ -32,12 +33,12 @@ class WritingSentenceFinishFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<TextView>(R.id.writing_sentence_finish_btn_sentence).setOnClickListener {
-            val intent = Intent(it.context , MainActivity::class.java)
+            //나중에 문장 확인하는 뷰 만들어지면 수정해야함 !
+            val intent = Intent(it.context , SentenceDetailViewActivity::class.java)
             it.context.startActivity(intent)
             act.finish()
         }
         view.findViewById<TextView>(R.id.writing_sentence_finish_btn_main).setOnClickListener {
-            //나중에 문장 확인하는 뷰 만들어지면 수정해야함 !
             val intent = Intent(it.context , MainActivity::class.java)
             it.context.startActivity(intent)
             act.finish()
