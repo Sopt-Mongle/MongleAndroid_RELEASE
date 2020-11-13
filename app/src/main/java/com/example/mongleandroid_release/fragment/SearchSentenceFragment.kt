c9f1325fa7ed4ab7b0e871db9b2931a66deb6682
jinyand <jiiin97@naver.com>
2020-11-14 02:59:18 +0900

    [Implement Features] 검색 아이템 클릭 리스너 구현

--- app/src/main/java/com/example/mongleandroid_release/fragment/SearchSentenceFragment.kt
+++ app/src/main/java/com/example/mongleandroid_release/fragment/SearchSentenceFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mongleandroid_release.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.mongleandroid_release.R
 import com.example.mongleandroid_release.activity.MainActivity.Companion.search_result
+import com.example.mongleandroid_release.activity.SentenceDetailViewActivity
 import com.example.mongleandroid_release.adapter.SearchSentenceAdapter
 import com.example.mongleandroid_release.network.RequestToServer
 import com.example.mongleandroid_release.network.data.response.ResponseSearchSentenceData
@@ -47,6 +49,15 @@ class SearchSentenceFragment : Fragment() {
                         searchSentenceAdapter = SearchSentenceAdapter(view!!.context, body.data)
                         rv_search_sentence.adapter = searchSentenceAdapter
                         searchSentenceAdapter.notifyDataSetChanged()
+
+                        searchSentenceAdapter.setItemClickListener(object : SearchSentenceAdapter.ItemClickListener{
+                            override fun onClick(view: View, position: Int) {
+                                val intent = Intent(context, SentenceDetailViewActivity::class.java)
+                                intent.putExtra("param", body.data[position].sentenceIdx)
+                                startActivity(intent)
+                            }
+
+                        })
                     }
                 } else {
                     fragment_search_sentence_cl_noresult.visibility = View.VISIBLE
