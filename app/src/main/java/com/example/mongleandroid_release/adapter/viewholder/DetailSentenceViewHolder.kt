64cfed6d1b657d2120bb035c58cf2a8189d678a8
jinyand <jiiin97@naver.com>
2020-11-15 02:55:38 +0900

    [Implement Features] 뷰 수정

--- app/src/main/java/com/example/mongleandroid_release/adapter/viewholder/DetailSentenceViewHolder.kt
+++ app/src/main/java/com/example/mongleandroid_release/adapter/viewholder/DetailSentenceViewHolder.kt
@@ -9,14 +9,14 @@ import com.example.mongleandroid_release.network.data.response.OtherSentence
 class DetailSentenceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
     val tv_theme_sentence = itemView.findViewById<TextView>(R.id.tv_theme_sentence)
     val tv_theme_curator = itemView.findViewById<TextView>(R.id.tv_theme_curator)
-    val tv_save_num = itemView.findViewById<TextView>(R.id.tv_save_num)
-    //val tv_like_num = itemView.findViewById<TextView>(R.id.tv_like_num)
+    val tv_likes_num = itemView.findViewById<TextView>(R.id.tv_likes_num)
+    val tv_saves_num = itemView.findViewById<TextView>(R.id.tv_saves_num)
 
     fun bind(otherSentence: OtherSentence) {
         tv_theme_sentence.text = otherSentence.sentence
         tv_theme_curator.text = otherSentence.writer
-        tv_save_num.text = otherSentence.saves.toString()
-        //tv_like_num.text = otherSentence.likes.toString()
+        tv_likes_num.text = otherSentence.likes.toString()
+        tv_saves_num.text = otherSentence.saves.toString()
     }
 
 }
