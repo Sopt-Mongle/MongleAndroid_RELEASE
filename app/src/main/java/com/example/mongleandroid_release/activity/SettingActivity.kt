920bc1cc5dc23c8317ddfbf80562a67ad5d77403
chloe73 <esom735@gmail.com>
2020-11-14 16:14:56 +0900

    [Implement Features] - 1:1 문의하기 눌렀을 때 메일 보내기 창으로 이동

--- app/src/main/java/com/example/mongleandroid_release/activity/SettingActivity.kt
+++ app/src/main/java/com/example/mongleandroid_release/activity/SettingActivity.kt
@@ -1,16 +1,30 @@
 package com.example.mongleandroid_release.activity
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mongleandroid_release.R
-import com.kyleduo.switchbutton.SwitchButton
+import kotlinx.android.synthetic.main.activity_setting.*
 
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+
+        // 1:1 문의하기 눌렀을 때 메일 보내기 창 뜨기
+        tv_cs_one.setOnClickListener(View.OnClickListener {
+            val email = Intent(Intent.ACTION_SEND)
+            email.type = "plain/text"
+            val address =
+                arrayOf("mongle.official@gmail.com")
+            email.putExtra(Intent.EXTRA_EMAIL, address)
+            email.putExtra(Intent.EXTRA_SUBJECT, "test@test")
+            email.putExtra(Intent.EXTRA_TEXT, "내용 미리보기 (미리적을 수 있음)")
+            startActivity(email)
+        })
 
     }
 
