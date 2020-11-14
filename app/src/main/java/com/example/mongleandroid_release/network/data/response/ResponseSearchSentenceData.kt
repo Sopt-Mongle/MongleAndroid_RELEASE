921aef82eafc2df8594226ba87d6fe4486259e9e
jinyand <jiiin97@naver.com>
2020-11-15 03:18:15 +0900

    [Implement Features] 문장검색 수정

--- app/src/main/java/com/example/mongleandroid_release/network/data/response/ResponseSearchSentenceData.kt
+++ app/src/main/java/com/example/mongleandroid_release/network/data/response/ResponseSearchSentenceData.kt
@@ -8,12 +8,12 @@ data class ResponseSearchSentenceData (
 )
 
 data class SearchSentence (
-    val sentenceIdx : String, // 문장 고유 idx
+    val sentenceIdx : Int, // 문장 고유 idx
     val sentence : String, // 문장 내용
-    val themeIdx : String, // 문장이 속한 테마 idx
+    val themeIdx : Int, // 문장이 속한 테마 idx
     val theme : String, // 문장이 속한 테마 이름
-    val likes : String, // 문장 좋아요 수
-    val saves : String, // 문장 북마크 수
+    val likes : Int, // 문장 좋아요 수
+    val saves : Int, // 문장 북마크 수
     val writer : String, // 문장 만든 사람 이름
     val writerImg : String, // 문장 만든 사람 프로필 사진
     val title : String, // 책 제목
