0e888dbc332fc143127cb6ae4ce6a143c188bfd7
chloe73 <esom735@gmail.com>
2020-11-16 19:47:56 +0900

    [Implement Features] - 테마에 문장 쓰기 flow 연결 2. 책 검색 뷰 만들기

---
+++ app/src/main/java/com/example/mongleandroid_release/activity/ThemeWritingSentenceBookActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mongleandroid_release.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mongleandroid_release.R
+
+class ThemeWritingSentenceBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_theme_writing_sentence_book)
+
+
+    }
+}
