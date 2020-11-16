10dcfca4b93a9db12c88ab8ae10e203445fce713
jinyand <jiiin97@naver.com>
2020-11-17 01:32:39 +0900

    [Implement Features] 내서재 구독여부 통신

--- app/src/main/java/com/example/mongleandroid_release/fragment/LibraryCuratorFragment.kt
+++ app/src/main/java/com/example/mongleandroid_release/fragment/LibraryCuratorFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mongleandroid_release.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,10 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mongleandroid_release.R
+import com.example.mongleandroid_release.activity.CuratorInfoActivity
 import com.example.mongleandroid_release.adapter.LibraryCuratorAdapter
 import com.example.mongleandroid_release.network.RequestToServer
 import com.example.mongleandroid_release.network.SharedPreferenceController
 import com.example.mongleandroid_release.network.data.LibraryCuratorData
+import com.example.mongleandroid_release.network.data.response.ResponseCuratorFollowedData
 import com.example.mongleandroid_release.network.data.response.ResponseLibraryCuratorData
 import kotlinx.android.synthetic.main.fragment_library_curator.*
 import retrofit2.Call
@@ -35,82 +38,9 @@ class LibraryCuratorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        libraryCuratorAdapter = LibraryCuratorAdapter(view.context)
-//        rv_library_curator.adapter = libraryCuratorAdapter
-//        loadDatas()
-
         requestLibraryCuratorData()
 
     }
-
-
-//    private fun loadDatas() {
-//        libraryCuratorData.apply {
-//
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//            add(
-//                LibraryCuratorData(
-//                    library_curator_img_profile = "https://cdn11.bigcommerce.com/s-p64yd5j10d/products/2233/images/2262/RAL-6019-Pastel-Green__95839.1505828303.500.750.jpg?c=2",
-//                    tv_library_curator_name = "예스리",
-//                    tv_library_curator_subcount = "15",
-//                    tv_library_curator_keyword = "피엠예슬"
-//                )
-//            )
-//
-//
-//
-//
-//        }
-//        libraryCuratorAdapter.data_cu = libraryCuratorData
-//        libraryCuratorAdapter.notifyDataSetChanged()
-//    }
 
     private fun requestLibraryCuratorData() {
         requestToServer.service.lookLibraryCurator(
@@ -131,9 +61,42 @@ class LibraryCuratorFragment : Fragment() {
                             libraryCuratorAdapter = LibraryCuratorAdapter(view!!.context, response.body()!!.data)
                             rv_library_curator.adapter = libraryCuratorAdapter
                             libraryCuratorAdapter.notifyDataSetChanged()
-//                        response.body().let { body->
-//
-//                        }
+
+                            libraryCuratorAdapter.setItemClickListener(object : LibraryCuratorAdapter.ItemClickListener{
+                                override fun onClickItem(view: View, position: Int) {
+                                    // 큐레이터 상세로 이동
+                                    val intent = Intent(context, CuratorInfoActivity::class.java)
+                                    intent.putExtra("param", response.body()!!.data[position].curatorIdx)
+                                    startActivity(intent)
+                                }
+
+                                override fun onClickSubscribe(view: View, position: Int) {
+                                    // 구독 클릭했을 때
+                                    requestToServer.service.getFollowIdx(
+                                            token = context?.let { SharedPreferenceController.getAccessToken(it) },
+                                            params = response.body()!!.data[position].curatorIdx
+                                    ).enqueue(object : Callback<ResponseCuratorFollowedData> {
+                                        override fun onFailure(call: Call<ResponseCuratorFollowedData>, t: Throwable) {
+                                            Log.e("통신실패", t.toString())
+                                        }
+
+                                        override fun onResponse(
+                                                call: Call<ResponseCuratorFollowedData>,
+                                                response: Response<ResponseCuratorFollowedData>
+                                        ) {
+                                            if (response.isSuccessful) {
+                                                if(response.body()!!.data) {
+                                                    Log.d("구독", "구독")
+                                                } else {
+                                                    Log.d("구독", "구독취소")
+                                                }
+                                            }
+
+                                        }
+                                    })
+                                }
+
+                            })
 
                         }
 
