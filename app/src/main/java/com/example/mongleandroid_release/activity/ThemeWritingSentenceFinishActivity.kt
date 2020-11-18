fe75404205765d5350589d91ebeaaa63f99f9d39
chloe73 <esom735@gmail.com>
2020-11-19 01:17:54 +0900

    [Implement Features] - 테마에 문장 쓰기 flow : 메인으로 돌아가기 액티비티 스택 정리 완료

--- app/src/main/java/com/example/mongleandroid_release/activity/ThemeWritingSentenceFinishActivity.kt
+++ app/src/main/java/com/example/mongleandroid_release/activity/ThemeWritingSentenceFinishActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mongleandroid_release.activity
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mongleandroid_release.R
@@ -19,9 +20,14 @@ class ThemeWritingSentenceFinishActivity : AppCompatActivity() {
         }
         // 메인으로 돌아가기
         theme_writing_sentence_finish_btn_main.setOnClickListener {
-            finish()
-//            val intent = Intent(this,MainActivity::class.java)
-//            startActivity(intent)
+
+            val intent = Intent(this,MainActivity::class.java)
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            } else {
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            startActivity(intent)
         }
     }
 }
